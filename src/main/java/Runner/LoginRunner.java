37b7d90d295052200b3a468d64564b41d09a1eff
Hardik Mehta <hardikmehta008@gmail.com>
2026-08-13 21:03:50 +0530

    add code for Hooks feature in cucumber

--- src/main/java/Runner/LoginRunner.java
+++ src/main/java/Runner/LoginRunner.java
@@ -6,14 +6,14 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "E:\\Intellij_IDEA_Projects\\Selenium_Cucumber_Beginner_Examples\\src\\main\\java\\Features\\hooks.feature", //the path of the feature file.
+        features = "E:\\Intellij_IDEA_Projects\\Selenium_Cucumber_Beginner_Examples\\src\\main\\java\\Features\\taggedhooks.feature", //the path of the feature file.
         glue = {"stepDefinations"}, //the path of the step defination file.
         plugin = { //to generate different types of reporting.
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
                 "json:target/cucumber-reports/cucumber.json"
         },
-        dryRun = false, //to check the mapping is proper between feature file & step defination file.
+        dryRun = true, //to check the mapping is proper between feature file & step defination file.
         monochrome = false //Display the console output in a proper readable format.
         //tags = "not (@SmokeTest or @RegressionTest) and @End2End"
         //strict :- it will be checking if any step is not defined in step defination file.
